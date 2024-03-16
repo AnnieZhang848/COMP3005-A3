1cb7ffdd8a06623d69f194e9bbd89e7ea2efd54f
Annie <anniezhang848@gmail.com>
2024-03-16 09:57:08 -0400

    Update Main.java

--- assignment3/src/main/java/org/example/Main.java
+++ assignment3/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+    // Class variables 
     public static String url = "jdbc:postgresql://localhost:5432/Assignment3";
     public static String user = "postgres";
     public static String password = "SQLpass";
@@ -14,12 +15,16 @@ public class Main {
 
     static void getAllStudents(){
         try { 
+            String[] tableHeaders = {"student_id", "first_name", "last_name", "email", "enrollment_date"};
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM students");
-            String[] tableHeaders = {"student_id", "first_name", "last_name", "email", "enrollment_date"};
+
+            // Print all headers            
             for(int i = 0; i<tableHeaders.length; i++){
                 System.out.print(tableHeaders[i] + "\t");
             }
+
+            // Print the "students" table line by line
             System.out.println();
             while(rs.next()){
                 for(int i = 0; i<tableHeaders.length; i++){
@@ -38,11 +43,14 @@ public class Main {
     static void addStudent(String first_name, String last_name, String email, String enrollment_date){
         try {
             Statement stmt = conn.createStatement();
+
+            // Fill out the INSERT statement using the user input
             String insertSQL = " INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES (";
             insertSQL += "'" + first_name + "',";
             insertSQL += "'" + last_name + "',";
             insertSQL += "'" + email + "',";
             insertSQL += "'" + enrollment_date + "')";
+
             int rowsInserted = stmt.executeUpdate(insertSQL);
             if (rowsInserted > 0) {
                 System.out.println("A new student was inserted successfully!");
@@ -56,9 +64,12 @@ public class Main {
     static void updateStudentEmail(int student_id, String new_email){
         try {
             Statement stmt = conn.createStatement();
+
+            // Fill out the UPDATE statement using the user input
             String insertSQL = " UPDATE students SET email = '";
             insertSQL += new_email + "' WHERE student_id = ";
             insertSQL += student_id;
+
             int studId = stmt.executeUpdate(insertSQL);
             if(studId > 0){
                 System.out.println("Email was updated successfully!");
@@ -72,8 +83,11 @@ public class Main {
     static void deleteStudent(int student_id){
         try {
             Statement stmt = conn.createStatement();
+
+            // Fill out the DELETE statement using the user input
             String insertSQL = " DELETE FROM students WHERE student_id = ";
             insertSQL += student_id;
+
             int studId = stmt.executeUpdate(insertSQL);
             if(studId > 0){
                 System.out.println("Student was deleted successfully!");
@@ -89,11 +103,30 @@ public class Main {
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection(url, user, password);
             if (conn != null) {
-                //boolean querrying = true;
-                // while (querrying){
-
-                // }
+                // Test 1: Add Students
+                /*
                 getAllStudents();
+                addStudent("Annie", "Zhang", "annie.zhang@example.com", "2023-11-06");
+                addStudent("Ashe", "Eijin", "ashe.eijin@example.com", "2023-03-16");
+                getAllStudents(); 
+                */
+
+                // Test 2: Update Emails
+                /*
+                getAllStudents();
+                updateStudentEmail(1, "john.doe@newemail.com");
+                updateStudentEmail(2, "jane.smith@newemail.com");
+                getAllStudents();
+                */
+
+                // Test 3: Delete Students
+                /*
+                getAllStudents();
+                deleteStudent(4);
+                deleteStudent(5);
+                getAllStudents();
+                */
+
             } else {
                 System.out.println("Failed to establish connection.");
             }
